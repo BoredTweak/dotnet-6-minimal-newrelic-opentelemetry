91f68044aaee31f60c45d845ceb90c67163901a2
BoredTweak <alex.elia37@gmail.com>
2022-03-05 12:29:09 -0800

    Adding open telemetry implementation

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # dotnet-6-minimal-newrelic-opentelemetry
 Demonstration of .NET 6 minimal APIs with New Relic's OpenTelemetry harvesting
+
+
+## New Relic
+
+- [Blog Post](https://newrelic.com/blog/best-practices/new-relic-opentelemetry-net)
+- [Documentation](https://docs.newrelic.com/docs/more-integrations/open-source-telemetry-integrations/opentelemetry/opentelemetry-quick-start/)
--- fruit-service/Program.cs
+++ fruit-service/Program.cs
@@ -1,10 +1,38 @@
 using fruit_service;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Logs;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new() { Title = "Fruit Service", Version = "v1" });
+});
+
+const string SERVICE_NAME = "fruit-service";
+const string SERVICE_VERSION = "1.0.0";
+const string OTLP_TRACING_ENDPOINT = "https://otlp.nr-data.net:4318/v1/traces";
+const string OTLP_METRIC_ENDPOINT = "https://otlp.nr-data.net:4318/v1/metrics";
+var newRelicApiKey = builder.Configuration.GetSection("NEW_RELIC").GetValue<string>("API_KEY");
+
+var resourceBuilder = ResourceBuilder.CreateDefault().AddService(serviceName: SERVICE_NAME, serviceVersion: SERVICE_VERSION).AddTelemetrySdk();
+
+builder.Services.AddOpenTelemetryTracing(b => {
+    b.SetResourceBuilder(resourceBuilder)
+    .AddOtlpExporter(options => options.Endpoint = new Uri(OTLP_TRACING_ENDPOINT))
+    .AddConsoleExporter()
+    .AddHttpClientInstrumentation()
+    .AddAspNetCoreInstrumentation();
+});
+
+builder.Services.AddOpenTelemetryMetrics(b => {
+    b.SetResourceBuilder(resourceBuilder)
+    .AddOtlpExporter(options => options.Endpoint = new Uri(OTLP_METRIC_ENDPOINT))
+    .AddConsoleExporter()
+    .AddHttpClientInstrumentation()
+    .AddAspNetCoreInstrumentation();
 });
 
 var app = builder.Build();
