1e172ddf261177f85c32aad1d339ca69f4c025f7
BoredTweak <alex.elia37@gmail.com>
2022-03-19 08:17:35 -0700

    Fixing opentel opt in

--- fruit-service/Program.cs
+++ fruit-service/Program.cs
@@ -1,5 +1,7 @@
+using System.Runtime.ExceptionServices;
 using fruit_service;
 using OpenTelemetry.Exporter;
+using OpenTelemetry.Logs;
 using OpenTelemetry.Metrics;
 using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
@@ -13,40 +15,31 @@ builder.Services.AddSwaggerGen(c =>
 
 const string SERVICE_NAME = "fruit-service";
 const string SERVICE_VERSION = "1.0.0";
-var otlpEndpoint = builder.Configuration.GetValue<string>("OTEL_EXPORTER_OTLP_ENDPOINT");
-var newRelicApiKey = builder.Configuration.GetSection("NEW_RELIC").GetValue<string>("API_KEY");
 var resourceBuilder = ResourceBuilder.CreateDefault().AddService(serviceName: SERVICE_NAME, serviceVersion: SERVICE_VERSION).AddTelemetrySdk();
+builder.Services.AddHttpClient();
 
 builder.Services.AddOpenTelemetryTracing(b => {
     b.SetResourceBuilder(resourceBuilder)
     .AddAspNetCoreInstrumentation()
+    .AddHttpClientInstrumentation()
     .AddConsoleExporter()
-    .AddOtlpExporter(options => {
-        options.Protocol = OtlpExportProtocol.HttpProtobuf;
-        options.Endpoint = new Uri(otlpEndpoint);
-        options.HttpClientFactory = () =>
-        {
-            HttpClient client = new HttpClient();
-            client.DefaultRequestHeaders.Add("api-key", newRelicApiKey);
-            return client;
-        };
-        });
+    .AddOtlpExporter(options =>
+    {
+        options.Endpoint = new Uri($"{Environment.GetEnvironmentVariable("OTEL_EXPORTER_OTLP_ENDPOINT")}");
+        options.Headers = Environment.GetEnvironmentVariable("OTEL_EXPORTER_OTLP_HEADERS");
+    });
 });
 
 builder.Services.AddOpenTelemetryMetrics(b => {
     b.SetResourceBuilder(resourceBuilder)
     .AddAspNetCoreInstrumentation()
     .AddConsoleExporter()
-    .AddOtlpExporter(options => {
-        options.Protocol = OtlpExportProtocol.HttpProtobuf;
-        options.Endpoint = new Uri(otlpEndpoint);
-        options.HttpClientFactory = () =>
-        {
-            HttpClient client = new HttpClient();
-            client.DefaultRequestHeaders.Add("api-key", newRelicApiKey);
-            return client;
-        };
-        });
+    .AddHttpClientInstrumentation()
+    .AddOtlpExporter(options =>
+    {
+        options.Endpoint = new Uri($"{Environment.GetEnvironmentVariable("OTEL_EXPORTER_OTLP_ENDPOINT")}");
+        options.Headers = Environment.GetEnvironmentVariable("OTEL_EXPORTER_OTLP_HEADERS");
+    });
 });
 
 var app = builder.Build();
@@ -66,4 +59,13 @@ app.MapGet("/fruits", () => {
     })
     .ToArray());
 });
+
+// Helps debug some opentel errors
+AppDomain.CurrentDomain.FirstChanceException +=
+            (object source, FirstChanceExceptionEventArgs e) =>
+            {
+                Console.WriteLine("FirstChanceException event raised in {0}: {1}",
+                    AppDomain.CurrentDomain.FriendlyName, e.Exception.Message);
+            };
+
 app.Run();
